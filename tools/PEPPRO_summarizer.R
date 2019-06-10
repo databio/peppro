b44d01e6cea5d2d3980572f7765d5a1b059b8ad2
Jason Smith <jps3dp@virginia.edu>
2019-06-10 15:08:28 -0400

    move summarizer outside main parser to work with Looper

---
+++ tools/PEPPRO_summarizer.R
@@ -0,0 +1,77 @@
+#! /usr/bin/env Rscript
+###############################################################################
+#6/10/19
+#Author: Jason Smith
+#PEPPRO_summarizer.R
+#
+#This program is meant to plot multiple library complexity curves on the 
+#same plot when called by looper summarize
+#
+#NOTES:
+#usage: Rscript tools/PEPPRO_summarizer.R 
+#       /path/to/project_config.yaml
+#
+#requirements: PEPPROr
+#
+###############################################################################
+version <- 0.1
+##### Load dependencies #####
+
+required_libraries <- c("PEPPROr")
+for (i in required_libraries) {
+    loadLibrary <- tryCatch (
+        {
+            suppressPackageStartupMessages(
+                suppressWarnings(library(i, character.only=TRUE)))
+        },
+        error=function(e) {
+            message("Error: Install the \"", i,
+                    "\" R package before proceeding.")
+            message('i.e. devtools::install_github("databio/peppro",',
+                    ' subdir="PEPPROr")')
+            return(NULL)
+        },
+        warning=function(e) {
+            message(e)
+            return(1)
+        }
+    )
+    if (length(loadLibrary)!=0) {
+        suppressWarnings(library(i, character.only=TRUE))
+    } else {
+        quit()
+    }
+}
+
+###############################################################################
+
+configFile <- opt_get_verb()
+prj        <- suppressWarnings(Project(configFile))
+# Produce output directory (if needed)
+dir.create(
+    suppressMessages(
+        file.path(config(prj)$metadata$output_dir, "summary")),
+    showWarnings = FALSE)
+
+# Plot combined library complexity curves for all samples in project
+cc <- paste(config(prj)$metadata$output_dir,
+            "results_pipeline",
+            samples(prj)$sample_name,
+            paste0("QC_", samples(prj)$genome),
+            paste0(samples(prj)$sample_name, "_preseq_yield.txt"),
+            sep="/")
+rc <- paste(config(prj)$metadata$output_dir,
+            "results_pipeline",
+            samples(prj)$sample_name,
+            paste0("QC_", samples(prj)$genome),
+            paste0(samples(prj)$sample_name, "_preseq_counts.txt"),
+            sep="/")
+plot_complexity_curves(ccurves = cc,
+    coverage = 0, read_length = 0,
+    real_counts_path = rc, ignore_unique = FALSE,
+    output_name = paste(config(prj)$metadata$output_dir,
+                        "summary",
+                        paste0(config(prj)$name, "_libComplexity"),
+                        sep="/"))
+
+###############################################################################
