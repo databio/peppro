fb11aaf4ed49bdbc36fe7362ea0c0c80256ca6d9
jpsmith5 <jasonsmith@virginia.edu>
2021-02-05 16:41:47 -0500

    fix file naming

--- docs/files/examples/paper/summary/PEPPRO collator_commands.sh
+++
@@ -1,8 +0,0 @@
-# Pipeline started at 06-15 13:09:14
-
-Rscript /scratch/jps3dp/tools/databio//peppro/tools/PEPPRO_summarizer.R /sfs/lustre/bahamut/scratch/jps3dp/tools/databio/ppqc/peppro_paper_rerun.yaml /project/shefflab/processed//peppro/paper/6.11.2020 /project/shefflab/processed//peppro/paper/6.11.2020/results_pipeline
-# Pipeline started at 06-16 09:04:20
-
-# Pipeline started at 06-16 09:05:10
-
-Rscript /scratch/jps3dp/tools/databio//peppro/tools/PEPPRO_summarizer.R /sfs/lustre/bahamut/scratch/jps3dp/tools/databio/ppqc/peppro_paper_rerun.yaml /project/shefflab/processed//peppro/paper/6.11.2020 /project/shefflab/processed//peppro/paper/6.11.2020/results_pipeline --new-start
