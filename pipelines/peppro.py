ce3c67884ca70e07ff4bd0ed7d2b66937733958b
jpsmith5 <jasonsmith@virginia.edu>
2026-02-19 07:03:42 -0500

    pause R2 trimmed check until file is made

--- pipelines/peppro.py
+++ pipelines/peppro.py
@@ -1413,8 +1413,8 @@ def _process_fastq(args, tools, res, read2, fq_file, outfolder):
     paired_end = args.paired_end
     if read2:
         pm.run([adapter_command, trim_command], trimmed_fq2,
-               follow=check_trim(processed_fastq, paired_end, trimmed_fq2,
-                                 fastqc_folder=fastqc_folder))
+               follow=lambda: check_trim(processed_fastq, paired_end, trimmed_fq2,
+                                         fastqc_folder=fastqc_folder))
         if args.adapter == "cutadapt":
             output_folder = os.path.join(outfolder, "cutadapt")
         else:
